a419e94f654b416ce4a2930a91863ad10c6931d3
DoxaNtow <dianasenakey@gmail.com>
2023-10-04 19:20:24 -0700

    two digits combination

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""prints all possible different combinations of two digits"""
+for numb1 in range(0, 10):
+    for numb2 in range(numb1 + 1, 10):
+        if numb1 == 8 and numb2 == 9:
+            print("{}{}".format(numb1, numb2))
+        else:
+            print("{}{}".format(numb1, numb2), end=", ")
