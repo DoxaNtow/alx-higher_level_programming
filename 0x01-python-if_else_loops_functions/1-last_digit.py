ad5a43fafe67d038d58eb63342cd9d07c2144c5c
DoxaNtow <dianasenakey@gmail.com>
2023-10-04 18:09:51 -0700

    pycodestyle check

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+
 if number < 0:
     last_dig = number % -10
 elif number >= 0:
@@ -12,4 +13,3 @@ elif last_dig == 0:
     print(f"Last digit of {number} is {last_dig} and is 0")
 else:
     print(f"Last digit of {number} is {last_dig} and is less than 6 and not 0")
-
