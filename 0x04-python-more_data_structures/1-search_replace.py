51cb845cdbde5aa117783977be643b3fc0aea60f
DoxaNtow <dianasenakey@gmail.com>
2023-10-11 13:09:13 -0700

    search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    result_list = list(my_list)
+    for i in range(len(result_list)):
+        if result_list[i] == search:
+            result_list[i] = replace
+    return result_list
