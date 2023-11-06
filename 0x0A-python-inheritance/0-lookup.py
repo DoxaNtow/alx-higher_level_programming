1d910a5d4b67f6f3f3666a0127208b65a63be313
DoxaNtow <dianasenakey@gmail.com>
2023-11-06 12:11:40 -0800

    lookup3

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
-"""
-This script defines a function for looking up object attributes.
-"""
+"""This script defines a function for looking up object attributes.."""
+
 
 def lookup(obj):
-    """
-    Return a list of all available attributes of an object.
-    
-    Args:
-        obj (object): The object for which attributes will be looked up.
-        
-    Returns:
-        list: A list of available attributes.
-    """
-    return dir(obj)
+    """Return a list of all available attributes of an object."""
+    return (dir(obj))
