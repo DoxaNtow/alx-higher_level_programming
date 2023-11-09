d6e1893e192bc1dc54f328d51c6d4bd8336f3fb1
DoxaNtow <dianasenakey@gmail.com>
2023-11-08 16:31:12 -0800

    class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""defines a function for converting a Python class to a JSON dictionary."""
+
+def class_to_json(obj):
+    """Convert a Python class instance to a dictionary representation."""
+    return obj.__dict__
